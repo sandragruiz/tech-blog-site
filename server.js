b6c7126a9e27bee5811440879eb47458de607783
unknown <sandra.gutierrez.ru@gmail.com>
2023-04-30 19:32:56 -0500

    added folders and files

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
